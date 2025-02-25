a3538aba037a1c181fa16cd3ab922ffa4d112d9e
Bharti Rani <bhartirani@digitalhealthsol.com>
2025-02-25 12:54:50 +0530

    Implemented sum for multiple numbers - Test passed

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,6 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    final numberList = numbers.split(',').map(int.parse).toList();
-    return numberList.reduce((a, b) => a + b);
+    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
   }
 }
