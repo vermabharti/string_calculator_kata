01c3a8624b10ee4307cb1b4f761ca37ac57adea0
Bharti Rani <bhartirani@digitalhealthsol.com>
2025-02-25 11:57:03 +0530

    Test: Return 0 for empty string

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:string_calculator_kata/string_calculator.dart';
+
+void main() {
+  final calculator = StringCalculator();
+
+  test('Empty string should return 0', () {
+    expect(calculator.add(""), 0);
+  });
+}
