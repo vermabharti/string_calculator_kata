00510b4ba996e7d1b8f1098b7d418697681aa6b6
Bharti Rani <bhartirani@digitalhealthsol.com>
2025-02-25 12:56:38 +0530

    Implemented custom delimiter support - Test passed

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,9 +1,18 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    return numbers
-        .split(RegExp(r'[,\n]'))
-        .map(int.parse)
-        .reduce((a, b) => a + b);
+
+    final delimiters = [',', '\n'];
+    String numberString = numbers;
+
+    if (numbers.startsWith("//")) {
+      final parts = numbers.split('\n');
+      final customDelimiter = parts[0].substring(2);
+      numberString = parts.sublist(1).join('\n');
+      delimiters.add(customDelimiter);
+    }
+
+    final regex = RegExp(delimiters.map(RegExp.escape).join('|'));
+    return numberString.split(regex).map(int.parse).reduce((a, b) => a + b);
   }
 }
