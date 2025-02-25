85ef2ceeb560edda4e716c3fae9ef4df28e33e54
Bharti Rani <bhartirani@digitalhealthsol.com>
2025-02-25 12:55:45 +0530

    Implemented newline delimiter support - Test passed

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,6 +1,9 @@
 class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    return numbers.split(',').map(int.parse).reduce((a, b) => a + b);
+    return numbers
+        .split(RegExp(r'[,\n]'))
+        .map(int.parse)
+        .reduce((a, b) => a + b);
   }
 }
